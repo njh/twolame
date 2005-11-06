5fc827b293df53c412dd14930ee6bef1583f5b9a
Nicholas J Humfrey <njh@aelius.com>
2005-11-06 00:44:02 +0000

    Check for pkg-config in autogen.sh
    git-svn-id: https://twolame.svn.sourceforge.net/svnroot/twolame/trunk@51 24616d55-7714-0410-b2f5-c000b2ba9aa2

--- autogen.sh
+++ autogen.sh
@@ -35,6 +35,23 @@ DIE=0
     DIE=1
 }
 
+(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool installed to compile $package."
+	echo "Download the appropriate package for your system,"
+	echo "or get the source from one of the GNU ftp sites"
+	echo "listed in http://www.gnu.org/order/ftp.html"
+	DIE=1
+}
+
+(pkg-config --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have pkg-config installed to compile $package."
+    echo "Download the appropriate package for your system,"
+    echo "or get the source from http://pkgconfig.freedesktop.org/"
+    DIE=1
+}
+
 
 if test "$DIE" -eq 1; then
     exit 1
