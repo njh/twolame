2bae9c6fd04cc820dedcb4b2fc79280f4b1f6752
Nicholas J Humfrey <njh@aelius.com>
2007-03-20 22:17:51 +0000

    Added Id: thingy
    git-svn-id: https://twolame.svn.sourceforge.net/svnroot/twolame/trunk@153 24616d55-7714-0410-b2f5-c000b2ba9aa2

--- libtwolame/util.h
+++ libtwolame/util.h
@@ -17,20 +17,22 @@
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *  
+ *
+ *  $Id:$
+ *
  */
  
 
 #ifndef	_UTIL_H_
 #define	_UTIL_H_
 
-// non-public prototypes
+/* non-public prototypes */
 const char* twolame_mpeg_version_name (int version);
 int twolame_get_bitrate_index (int bitrate, TWOLAME_MPEG_version version);
 int twolame_get_samplerate_index (long sample_rate);
 int twolame_get_version_for_samplerate (long sample_rate);
 
-#endif // _UTIL_H_
+#endif /* _UTIL_H_ */
 
 
-// vim:ts=4:sw=4:nowrap:
+/* vim:ts=4:sw=4:nowrap: */
