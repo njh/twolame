d82deb36ad75a5cff919da4970582768f9ef06f3
Nicholas Humfrey <njh@aelius.com>
2017-02-15 20:34:39 +0000

    Removed $Id$ and $Log$ lines

--- frontend/frontend.h
+++ frontend/frontend.h
@@ -17,8 +17,6 @@
  *	License along with this library; if not, write to the Free Software
  *	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *  $Id: frontend.h 156 2007-03-20 23:57:35Z nhumfrey $
- *
  */
 
 #include <sndfile.h>
