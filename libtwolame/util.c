b6ec24ddda827668baf3563fd80db6115a2e35f1
Nicholas Humfrey <njh@aelius.com>
2017-11-27 20:27:48 +0000

    Revert "Simplify loop in twolame_get_bitrate_index" It is causing tests to fail.
    This reverts commit f66efb84cf7a3ac630c7cfb7f542891da33e7d2e.

--- libtwolame/util.c
+++ libtwolame/util.c
@@ -85,9 +85,12 @@ int twolame_get_bitrate_index(int bitrate, TWOLAME_MPEG_version version)
         return -1;
     }
 
-    while (++index < 15)
+    while (index < 15) {
         if (bitrate_table[version][index] == bitrate)
             break;
+        else
+            ++index;
+    }
 
     if (index == 15) {
         fprintf(stderr,
